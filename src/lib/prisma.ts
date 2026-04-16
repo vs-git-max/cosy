6523d7aa9390185f01b6cf27e195c96181d8ba26
sam chris <vsgitsam25@gmail.com>
2026-04-16 18:21:28 +0300

    small changes here and there

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client/extension";
 import "dotenv/config";
+import { PrismaClient } from "../../generated/prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 

a9ee7833f43b62b48c45c2b4a3f3553a0e86f00a
vs-git-max <vsgitmax25@gmail.com>
2026-04-19 19:54:18 +0300

    added the pg

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,8 @@
-import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle(pool);
